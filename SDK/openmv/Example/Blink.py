8c9da30b8712e197f40cf49c04291f550bfa1964
WeActTC <WeAct_TC@163.com>
2020-07-05 15:23:46 +0800

    Add openmv example && update WeAct Studio Download Tool

---
+++ SDK/openmv/Example/Blink.py
@@ -0,0 +1,15 @@
+import pyb, micropython
+import time
+micropython.alloc_emergency_exception_buf(100)
+
+class Foo(object):
+    def __init__(self, timer, led):
+        self.led = led
+        timer.callback(self.cb)
+    def cb(self, tim):
+        self.led.toggle()
+
+blue = Foo(pyb.Timer(2, freq=2), pyb.LED(1)) # LED(1) -> PE3
+
+while True:
+    time.sleep(1000)
---
+++ SDK/openmv/Example/uart1_test.py
@@ -0,0 +1,16 @@
+from pyb import UART
+from pyb import LED
+from time import sleep
+
+uart1 = UART(1, 9600)  # PA9,PA10
+uart1.write('hello')
+
+while True:
+    rec=uart1.read(50)
+    if rec != None:
+        print(rec)
+        for t in rec:
+            print('%#x '%t,end='')
+        print('\r\n')
+    sleep(100)
+    LED(1).toggle()
