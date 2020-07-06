f81e6ed07a74b6059dece3356f9f8aa40fb9d221
WeActTC <WeAct_TC@163.com>
2020-07-06 15:06:35 +0800

    Open SDK/HAL/03-LCD_Test SWD Debug Port Update Soft/WeAct Studio Download Tool

--- SDK/HAL/03-LCD_Test/Src/gpio.c
+++ SDK/HAL/03-LCD_Test/Src/gpio.c
@@ -35,6 +35,8 @@
      PC15-OSC32_OUT (OSC32_OUT)   ------> RCC_OSC32_OUT
      PH0-OSC_IN (PH0)   ------> RCC_OSC_IN
      PH1-OSC_OUT (PH1)   ------> RCC_OSC_OUT
+     PA13 (JTMS/SWDIO)   ------> DEBUG_JTMS-SWDIO
+     PA14 (JTCK/SWCLK)   ------> DEBUG_JTCK-SWCLK
 */
 void MX_GPIO_Init(void)
 {
@@ -45,6 +47,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(E3_GPIO_Port, E3_Pin, GPIO_PIN_RESET);
